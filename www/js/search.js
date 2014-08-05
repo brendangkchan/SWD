7e9c95dfd15f8af1e226a16b8b70ac314c7bfcc6
Brendan Chan <brendangkchan@gmail.com>
2014-08-05 03:27:38 -0700

    Initial Results List

--- www/js/search.js
+++ www/js/search.js
@@ -1,36 +1,97 @@
-var app = angular.module('search', ['ionic'])
+var app = angular.module('search', ['ionic', 'ngAnimate'])
 
-app.controller('SearchCtrl', function($scope, $ionicModal) {
 
-	$ionicModal.fromTemplateUrl('search-modal.html', {
-		scope: $scope,
-		animation: 'slide-in-left'
-	}).then(function(modal) {
-		$scope.modal = modal;
-	});
+// Dummy Search Results
 
-	$scope.openModal = function() {
-		$scope.modal.show();
+app.factory("Results", function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var results = [
+    { id: 0, title: 'Unbroken: A World War II Story of Survival, Resilience, and Redemption', author: 'Laura Hillenbrand', icon: 'img/test/book1.jpg'},
+    { id: 1, title: 'Diagnostic and Statistical Manual of Mental Disorders', author: 'American Psychiatric Association', icon: 'img/test/book2.jpg'},
+    { id: 2, title: 'StrengthsFinder 2.0', author: 'Tom Rath', icon: 'img/test/book3.jpg'},
+    { id: 3, title: 'Publication Manual of the American Psychological Association', author: 'American Psychiatric Association', icon: 'img/test/book4.jpg'}
+  ];
+
+
+  //return messages;
+
+  return {
+    all: function() {
+      return results;
+    },
+    get: function(resultId) {
+      // Simple index lookup
+      return friends[resultId];
+    }
+  }
+});
+
+// Search Controller
+
+app.controller('SearchCtrl', function($scope, Results) {
+
+	var showResultsButton = false;
+	var showModal = false;
+	$scope.query = "";
+
+	// Fake results from factory
+	$scope.results = Results.all();
+
+	$scope.setShowModal = function () {
+		this.showModal = true;
+	}
+
+	$scope.search = function (query) {
+		this.showResultsButton = true;
+		this.showModal = true;
+		this.query = $scope.query;
+		console.log("Query is: " + $scope.query);
+		//this.openModal();
 	};
-	$scope.closeModal = function() {
-		$scope.modal.hide();
-	};
 
-	//Cleanup the modal when we're done with it!
-	$scope.$on('$destroy', function() {
-	$scope.modal.remove();
-	});
-	// Execute action on hide modal
-	$scope.$on('modal.hidden', function() {
-	// Execute action
-	});
-	// Execute action on remove modal
-	$scope.$on('modal.removed', function() {
-	// Execute action
-	});
+	$scope.setHasResults = function() {
+		this.showResultsButton = true;
+	}
+});
+
+// Blank modal with query
+app.directive('modalDialog', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      show: '=',
+      queryInput: '=query'
+    },
+    //replace: true, // Replace with the template below
+    transclude: true, // we want to insert custom content inside the directive
+    link: function(scope, element, attrs) {
+      scope.dialogStyle = {};
+      if (attrs.width)
+        scope.dialogStyle.width = attrs.width;
+      if (attrs.height)
+        scope.dialogStyle.height = attrs.height;
+      scope.hideModal = function() {
+        scope.show = false;
+      };
+    },
+    templateUrl: 'templates/modalDialog.html'
+  };
 });
 
 
+// Directive for show after return input, ng-show = true if variable true
+app.directive("enterShowResults", function () {
+	return function (scope, element, attrs) {
+		element.bind("ngEnter", function () {
+			scope.setHasResults();
+		})
+	}
+})
+
+
+// Directive to for ng-enter, calls function on enter pressed
 app.directive('ngEnter', function() {
     return function(scope, element, attrs) {
         element.bind("keydown keypress", function(event) {
@@ -45,39 +106,3 @@ app.directive('ngEnter', function() {
     };
 });
 
-/*	// Triggered by search entry
-	$scope.showPopup = function() {
-		$scope.data = {}
-
-		// Custome popup
-		var myPopup = $ionicPopup.show({
-		    templateUrl: 'search-popup.html',
-		    title: 'Enter Wi-Fi Password',
-		    subTitle: 'Please use normal things',
-		    scope: $scope,
-		    buttons: [
-		      { text: 'Cancel' },
-		      {
-		        text: '<b>Save</b>',
-		        type: 'button-positive',
-		        onTap: function(e) {
-		          if (!$scope.data.wifi) {
-		            //don't allow the user to close unless he enters wifi password
-		            e.preventDefault();
-		          } else {
-		            return $scope.data.wifi;
-		          }
-		        }
-		      },
-		    ]
-		  });
-
-		  myPopup.then(function(res) {
-		    console.log('Tapped!', res);
-		  });
-
-		  // Close after timeout
-		  // $timeout(function() {
-		  //    myPopup.close(); 
-		  // }, 3000);
-	}*/
